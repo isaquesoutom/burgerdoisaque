80276a8daf657336a6445f878f66aa8130b8db84
isaquesoutom <isaque.soutom@gmail.com>
2021-03-04 14:53:54 -0300

    envio de imagem

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <h1>burger do ze</h1>
 burger do ze<br>
-Hoje a aula esta sendo muito legal 
+Hoje a aula esta sendo muito legal<br>
+<img src="https://blog.saipos.com/wp-content/uploads/2018/07/hamburgueria1-SAIPOS-Sistema-para-Restaurantes.png">  
